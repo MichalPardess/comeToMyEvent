3c3aa69823752020ffe83fd7431f1c0c2a0934bb
michal-lev-pardess <michallevmail@gmail.com>
2023-10-29 01:04:08 +0300

    password protect

--- server/services/render.js
+++ server/services/render.js
@@ -52,8 +52,32 @@ exports.rsvp_C=(req,res)=>{
     res.render('rsvp_C');
 }
 exports.guest_list=(req,res)=>{
+    const reject = () => {
+        res.setHeader("www-authenticate", "Basic");
+        res.sendStatus(401);
+      };
+    
+      const authorization = req.headers.authorization;
+    
+      if (!authorization) {
+        return reject();
+      }
+    
+      const [username, password] = Buffer.from(
+        authorization.replace("Basic ", ""),
+        "base64"
+      )
+        .toString()
+        .split(":");
+    
+      if (!(username === "shriki" && password === "moshe")) {
+        return reject();
+      }
+    
+    
     axios.get('http://localhost:3000/api/participents')
         .then(function(response){
+            
             res.render('guest_list', {participents:response.data});
         })
         .catch(err =>{
